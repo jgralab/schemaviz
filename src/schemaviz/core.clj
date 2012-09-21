c4e661a0f5827be39c4b331baa6f36fe40d95e8f
Tassilo Horn <tsdh@gnu.org>
2012-09-21 14:08:45 +0200

    add some docs

--- src/schemaviz/core.clj
+++ src/schemaviz/core.clj
@@ -13,13 +13,34 @@
   (memoize (fn [v] (gensym "v"))))
 
 (def ^:dynamic *options*
-  "Default options to use.  May be overridden to customize the layout."
+  "Default options to use.  May be overridden to customize the layout by
+  providing an own options map to `visualize-schema` whose entries are merged
+  with the default opts.
+
+  The meaning of the options are as follows:
+
+    :rankdir                 See http://www.graphviz.org/content/attrs#drankdir
+    :ranksep                 See http://www.graphviz.org/content/attrs#dranksep
+    :fontname                See http://www.graphviz.org/content/attrs#dfontname
+    :graphclass-fill-color   The fill color of the graphclass node
+    :edgeclass-fill-color    The fill color of edgeclass nodes
+    :edgeclass-weight        The weight of edgeclass edges.  If it is higher
+                             than :specialization-weight, then edgeclass edges
+                             are tried to be shorter and more straight than
+                             specialization edges.
+    :specialization-color    The color of specialization edges
+    :specialization-weight   See :edgeclass-weight
+    :enumdomain-fill-color   The fill color of enumdomain nodes
+    :recorddomain-fill-color The fill color of recorddomains"
   {:rankdir "BT"
    :ranksep "3.0"
    :fontname "Helvetica"
+   :graphclass-fill-color "green"
    :vertexclass-fill-color "lightblue"
    :edgeclass-fill-color "khaki"
+   :edgeclass-weight 1
    :specialization-color "gray"
+   :specialization-weight 2
    :enumdomain-fill-color "goldenrod"
    :recorddomain-fill-color "lightsalmon"})
 
@@ -50,15 +71,8 @@
 
 ;;## EdgeClasses
 
-(defn emit-inc-class [where ic]
-  (str where "label=\""
-       (when-let [role (value ic :roleName)]
-         (str role "\\n"))
-       (when-let [redefs (seq (iseq ic 'Redefines :out))]
-         (str "{redefines " (str/join ", "
-                                      (map #(value (omega %) :roleName)
-                                           redefs))
-              "}\\n"))
+(defn emit-inc-class [ic]
+  (str "label=\""
        (let [min (Integer/valueOf (value ic :min))
              max (Integer/valueOf (value ic :max))]
          (cond
@@ -68,6 +82,13 @@
                                    "*"
                                    max)
                      ")")))
+       (when-let [role (value ic :roleName)]
+         (str "\\l" role))
+       (when-let [redefs (seq (iseq ic 'Redefines :out))]
+         (str "\\l{redefines " (str/join ", "
+                                      (map #(value (omega %) :roleName)
+                                           redefs))
+              "}\\n"))
        "\""))
 
 (defn emit-edge-class-1 [id ec]
@@ -88,14 +109,18 @@
         fvc (make-id (adj fic :targetclass))
         tvc (make-id (adj tic :targetclass))]
     (str (emit-edge-class-1 id ec)
-         "  " fvc " -> " id  "[dir=both, arrowhead=none, arrowtail="
+         "  " fvc " -> " id  " [dir=both, arrowhead=none, "
+         "weight=" (:edgeclass-weight *options*)
+         ", arrowtail="
          (cond
           (= tak (enum-constant ec 'structure.AggregationKind.COMPOSITE)) "diamond"
           (= tak (enum-constant ec 'structure.AggregationKind.SHARED))    "ediamond"
           :else "none") ", "
-         (emit-inc-class "tail" fic)
+         (emit-inc-class fic)
          "];\n"
-         "  " id  " -> " tvc "[" (emit-inc-class "head" tic)  "];\n")))
+         "  " id  " -> " tvc " ["
+         "weight=" (:edgeclass-weight *options*) ", "
+         (emit-inc-class tic)  "];\n")))
 
 (defn emit-edge-classes [sg]
   (apply str (map emit-edge-class (vseq sg 'EdgeClass))))
@@ -106,6 +131,7 @@
   (let [f (make-id (alpha s))
         t (make-id (omega s))]
     (str "  " f " -> " t " [color=" (:specialization-color *options*)
+         ", weight=" (:specialization-weight *options*)
          ", arrowhead=empty];\n")))
 
 (defn emit-specializations [sg]
@@ -134,25 +160,38 @@
 
 (defn emit-custom-domains-cluster [sg]
   (when-let [ds (vseq sg '[EnumDomain RecordDomain])]
-    (str "  subgraph clusterCustomDomains {\n"
-         (apply str (map (fn [d]
+    (apply str (map (fn [d]
                            (if (has-type? d 'EnumDomain)
                              (emit-enum-domain d)
                              (emit-record-domain d)))
-                         ds))
-         "  }")))
+                         ds))))
+
+;;## The graph class
+
+(defn emit-graph-class [sg]
+  (let [gc (the (vseq sg 'GraphClass))]
+    (str "  " (make-id gc) " [fillcolor=" (:graphclass-fill-color *options*)
+         ", style=filled, shape=record, label=\"{{«graphclass»\\n"
+         (value gc :qualifiedName) "}"
+         (when (seq (iseq gc 'HasAttribute))
+           (apply str "|" (map (partial emit-attribute "\\l")
+                               (adjs gc :attribute))))
+         "}\""
+         "];\n")))
 
 ;;## Main
 
 (defn visualize-schema
-  "Prints a visualization of Schema or SchemaGraph s to the file f."
+  "Prints a dot visualization of Schema or SchemaGraph `s` to the file `f`.
+  For a descriptions of `options`, see `*options*`."
   ([s f]
      (visualize-schema s f *options*))
-  ([s f opts]
-     (binding [*options* (merge *options* opts)]
+  ([s f options]
+     (binding [*options* (merge *options* options)]
        (let [sg (if (instance? Schema s)
                   (schema-graph s)
                   s)
+             gc (emit-graph-class sg)
              vcs (emit-vertex-classes sg)
              ecs (emit-edge-classes sg)
              specializations (emit-specializations sg)
@@ -161,16 +200,23 @@
                (str "digraph Extracted {\n"
                     "  rankdir=" (:rankdir *options*) ";\n"
                     "  ranksep=" (:ranksep *options*) ";\n"
-                    "  node [fontname=" (:fontname *options*) "];"
-                    "  edge [fontname=" (:fontname *options*) "];"
+                    "  node [fontname=" (:fontname *options*) "];\n"
+                    "  edge [fontname=" (:fontname *options*) "];\n"
+                    gc
                     vcs
                     ecs
                     specializations
+                    "  subgraph clusterCustomDomains {\n"
                     doms
+                    "}\n"
                     "}\n"))))))
 
-(visualize-schema
+#_(visualize-schema
  (load-schema "/home/horn/Repos/uni/jgralab/src/de/uni_koblenz/jgralab/schema/GrumlSchema.tg")
+ "/home/horn/test.dot")
+
+#_(visualize-schema
+ (load-schema "/home/horn/Repos/uni/grabaja/java5.tg")
  "/home/horn/test.dot")
 
 #_(visualize-schema
